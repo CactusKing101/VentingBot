f77b6fa8a3c91a52c7ccce52df07abbdd8b61dc2
CactusKing101 <chalkking17@gmail.com>
2021-04-19 12:59:54 -0400

    main

---
+++ index.js
@@ -0,0 +1,49 @@
+const Discord = require('discord.js');
+const fs = require('fs');
+const data = require('./general/data.json');
+const token = require('./general/token.json');
+const config = require('./general/config.json');
+const client = new Discord.Client();
+var main = data.main;
+var id = data.id;
+
+client.once('ready', () => {
+  client.user.setActivity('dm to vent');
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on('message', async msg => {
+  const ventCh = client.channels.cache.get('833730808686575626');
+  const guild = client.guilds.cache.get('830495072876494879');
+  if (msg.channel.type == 'dm') {
+    let banned = false;
+    for(let i = 0; i < config.banned.length; ++i) {
+
+    }
+    if (banned) return msg.channel.send('Sorry you have been banned from using this bot. If you think this is a mistake or want to apeal contact CactusKing101#2624. Depression and suicide is not a joke and if you feel you need help please call a suicide hotline.\nhttps://www.opencounseling.com/suicide-hotlines');
+    try {
+      const webhooks = await ventCh.fetchWebhooks();
+      const webhook = webhooks.first();
+      
+      if (webhook == null) return msg.channel.send('Error:\nNo webhooks found!');
+      main.push([++id, msg.author.tag, msg.author.id]);
+      var embed = new Discord.MessageEmbed().setDescription(msg.content).setAuthor('Anonymous Venter', guild.iconURL()).setColor('#9e9d9d').setFooter(`Id: ${id}`);
+      await webhook.send('[Venting]', {
+        username: 'Anonymous Venter',
+        avatarURL: guild.iconURL(),
+        embeds: [embed],
+      });
+      var tempData = {
+        main: main,
+        id: id
+      };
+      let json = JSON.stringify(tempData);
+      fs.writeFileSync('general/data.json', json);
+      console.log(id);
+    } catch (error) {
+      console.warn(error);
+    }
+  }
+});
+
+client.login(token.main);
